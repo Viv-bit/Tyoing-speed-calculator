7b3a5b5032a09db129c25d265932321ac1df81ba
Viv-bit <vivian.morenikenji@gmail.com>
2020-09-14 00:24:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  
+
+  const [text, setText] = useState("")
+
+  function handleChange(event) {
+    const {value} = event.target
+    setText(value)
+  }
+  
+  function countWords(text) {
+    const wordsArr = text.trim().split(" ")
+    return wordsArr.filter(words => words !== "").length;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Check How Fast You Type</h1>
+      <textarea onChange={handleChange} value={text} />
+      <h4>Time Remaining: seconds</h4>
+      <button onClick={() => console.log(countWords(text))}>Start</button>
+      <h1>Word Count:</h1>
+      <h3>Words per Second:</h3>
     </div>
   );
 }
